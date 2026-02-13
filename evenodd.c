eece3e6b5d56330bac1e8f228af0610e3d633659
Silent Watcher <132751706+Otakuboi0@users.noreply.github.com>
2026-02-13 11:11:43 +0530

    Add files via upload
    Check whether  a no is even or odd

---
+++ evenodd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h> 
+int main()
+{
+    int a ;
+    printf("enter the number of a =");
+    scanf("%d",&a);
+if (a%2==0)
+{
+    printf("even");
+}
+else
+{
+    printf("odd");
+} 
+return 0;
+}
